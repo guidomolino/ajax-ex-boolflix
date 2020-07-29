902d48640e1a7aff87c0401ac38a9fc50fcd8948
Guido <molido@hotmail.it>
2020-07-29 16:53:26 +0200

    Milesone2 v3

--- js/script.js
+++ js/script.js
@@ -34,12 +34,17 @@ function searchFilm(ricercaUtente, apiKey) {
       if (filmList) {
 
         var template = $("#film-template").html();
-        var compiled = Handlebars.compile(template);
         var target = $("#filmList");
+        var compiled = Handlebars.compile(template);
 
         for (var i = 0; i < filmList.length; i++) {
           var film = filmList[i];
-          starredVote(film, target);
+
+          var vote = film["vote_average"];
+          var language = film["original_language"];
+          film.stars = starredVote(vote);
+          film.flag = languageIcon(language);
+
           var filmHTML = compiled(film);
           target.append(filmHTML);
         }
@@ -73,13 +78,18 @@ function searchTV(ricercaUtente, apiKey) {
 
       if (tvList) {
 
+        var target = $("#tvList");
         var template = $("#tv-template").html();
         var compiled = Handlebars.compile(template);
-        var target = $("#tvList");
 
         for (var i = 0; i < tvList.length; i++) {
           var tv = tvList[i];
-          starredVote(tv, target);
+
+          var vote = tv["vote_average"];
+          var language = tv["original_language"];
+          tv.stars = starredVote(vote);
+          tv.flag = languageIcon(language);
+
           var tvHTML = compiled(tv);
           target.append(tvHTML);
         }
@@ -95,18 +105,40 @@ function searchTV(ricercaUtente, apiKey) {
   })
 }
 
-function starredVote(oggetto, target) {
-  var vote = oggetto["vote_average"];
+// funzione voti in stelle
+function starredVote(vote) {
+
   // numero stelle = voto / 2 arrotondato per eccesso
   var starsNumber = Math.round(vote/2);
+  var starsListHTML = '';
+
   // stampo stelle in base al numero
-
   for (var i = 0; i < starsNumber; i++) {
-    var stelle =
-    var voteTemplate = $("#vote-template").html();
-    target.append(voteTemplate);
+    starsListHTML += '<i class="fas fa-star"> </i>'
   }
+  return starsListHTML;
+}
 
+// funzione bandiere lingue
+function languageIcon(language) {
+  // it, en, fr, de, es, cn
+  var flagIcon = '';
+  if (language == "en") {
+    flagIcon += '<img src="img/flag-en.png" alt="">'
+  }else if (language = "it") {
+    flagIcon += '<img src="img/flag-it.png" alt="">'
+  }else if (language = "es") {
+    flagIcon += '<img src="img/flag-es.png" alt="">'
+  }else if (language = "de") {
+    flagIcon += '<img src="img/flag-de.png" alt="">'
+  }else if (language = "cn") {
+    flagIcon += '<img src="img/flag-cn.png" alt="">'
+  }else if (language = "fr") {
+    flagIcon += '<img src="img/flag-fr.png" alt="">'
+  }else {
+    flagIcon += language
+  }
+  return flagIcon;
 }
 
 
